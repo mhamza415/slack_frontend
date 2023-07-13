7b34f3056fa5402aab7909215bd3b3a9ccdafc78
zain007A <134587852+zain007A@users.noreply.github.com>
2023-07-13 20:28:05 +0500

    Updated main.js

--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from "./App.vue";
 import axios from "axios";
 import router from "./router";
 createApp(App).use(router).mount("#app");
-axios.defaults.baseURL = "https://ruby-inquisitive-pike.cyclic.app/";
+// axios.defaults.baseURL = "https://ruby-inquisitive-pike.cyclic.app/";
+axios.defaults.baseURL = "http://172.16.4.110:6090/";
